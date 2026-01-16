512c78ec8d2882eca4a749ff6c780f8c0207e2e4
shyam <khandareshyam2001@gmail.com>
2026-01-16 12:29:13 +0530

    chore: initialize repo and add .gitignore for Java 21 upgrade

---
+++ src/main/java/com/metro/ticket/MetroTicketApplication.java
@@ -0,0 +1,18 @@
+package com.metro.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroTicketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MetroTicketApplication.class, args);
+    }
+}
+
+
+
+
+
+
+
